c37fd82cab62630a20cabbfcefc5847a17222637
VietAnh3004 <anhanhdeptrai304@gmail.com>
2025-10-07 16:09:02 +0700

    init Springboot

---
+++ BE/src/main/java/com/smartmarket/danentang/DanentangApplication.java
@@ -0,0 +1,13 @@
+package com.smartmarket.danentang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanentangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanentangApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/smartmarket/danentang/DanentangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartmarket.danentang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanentangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
